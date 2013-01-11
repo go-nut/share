85703891ed3a6f6384e7890180a7677edd18ddea
Joseph Pond <joe@josephpond.com>
2013-01-10 23:39:07 -0500

    init

---
+++ main.go
@@ -0,0 +1,199 @@
+package main
+
+
+import (
+  "html"
+  "flag"
+  "net"
+  "net/http"
+  "archive/tar"
+//  "compress/gzip"
+  "log"
+  "os"
+  "io"
+)
+
+var (
+  ip string
+  port string
+  count int
+  requests int
+//  self bool
+  errLog *log.Logger
+  outLog *log.Logger
+  l net.Listener
+  fileList []string
+)
+
+func init() {
+  flag.StringVar(&port, "p", "8080", "Port to serve on")
+  flag.IntVar(&count, "c", 1, "Times to serve")
+//  flag.BoolVar(&self, "s", false, "Serve self")
+  errLog = log.New(os.Stderr, "Error ", 2)
+  outLog = log.New(os.Stdout, "Share: ", 1)
+
+
+  requests = 0
+}
+
+func main() {
+
+  a, err := net.InterfaceAddrs()
+  logErr(err)
+  _ip, _, err := net.ParseCIDR(a[len(a)-1].String())
+  flag.StringVar(&ip, "ip", _ip.String(), "IP to serve from")
+
+  flag.Parse()
+
+  host := ip + ":" + port
+  outLog.Println("Serving on http://" + host)
+
+  fileList = flag.Args()
+  if len(fileList) == 0 {
+    fileList = append(fileList, ".")
+  }
+
+  laddr, err := net.ResolveTCPAddr("tcp", host)
+  if err != nil {
+    errLog.Fatalln(err)
+  }
+
+  l, err = net.ListenTCP("tcp", laddr)
+  if err != nil {
+    errLog.Fatalln(err)
+  }
+
+  http.HandleFunc("/", handler)
+  err = http.Serve(l, nil)
+  if err != nil {
+    errLog.Fatalln(err)
+  }
+  outLog.Println("closing program")
+}
+
+func logErr(err error) {
+  if err != nil {
+    errLog.Fatalln(err)
+  }
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+  outLog.Println("starting handler")
+  if requests >= count {
+    return
+  }
+  for a, b := range r.Header {
+    outLog.Print("name: " +a)
+    outLog.Println(b)
+  }
+  if q := html.EscapeString(r.URL.Path); q != "/download.tar.gz" {
+    http.Redirect(w, r, "download.tar.gz", 302)
+    outLog.Print("Redirecting: " + q)
+    return
+  }
+
+  w.Header().Set("Content-Type", "application/octet-stream")
+  writer := w.(io.Writer)
+  outLog.Println("Serving to " + r.Host)
+  if err := writeArchive(fileList, writer); err != nil {
+    errLog.Fatalln(err)
+  }
+  w.Flush()
+  outLog.Println("Served to " + r.Host + " complete")
+
+  requests++
+  if requests >= count {
+    defer closeConn()
+  }
+  outLog.Println("ending handler")
+}
+
+func iterDir(dirPath string, tw *tar.Writer) error {
+  dir, err := os.Open( dirPath )
+  if err != nil {
+    return err
+  }
+  defer dir.Close()
+
+  fis, err := dir.Readdir( 0 )
+  if err != nil {
+    return err
+  }
+
+  for _, fi := range fis {
+    curPath := dirPath + "/" + fi.Name()
+    if fi.IsDir() {
+      if err := iterDir(curPath, tw); err != nil {
+        return err
+      }
+    } else {
+      if err := tarWrite(curPath, tw, fi); err != nil {
+        return err
+      }
+    }
+  }
+  return nil
+}
+
+func tarWrite(path string, tw *tar.Writer, fi os.FileInfo) error {
+  file, err := os.Open(path)
+  if err != nil {
+    return err
+  }
+  defer file.Close()
+
+  outLog.Println(path)
+  h := new(tar.Header)
+  h.Name = path
+  h.Size = fi.Size()
+  h.Mode = int64(fi.Mode())
+  h.ModTime = fi.ModTime()
+
+  err = tw.WriteHeader(h)
+  if err != nil {
+    return err
+  }
+  _, err = io.Copy(tw, file)
+  if err != nil {
+    return err
+  }
+  return nil
+}
+
+func writeArchive(paths []string, w io.Writer) error {
+//  gz := gzip.NewWriter(w)
+//  defer gz.Close()
+
+  tw := tar.NewWriter(w)
+  defer tw.Close()
+
+  for _, path := range paths {
+    f, err := os.Open(path)
+    if err != nil {
+      return err
+    }
+
+    stat, err := f.Stat()
+    if err != nil {
+      return err
+    }
+
+    if stat.IsDir() {
+      if err := iterDir(path, tw); err != nil {
+        return err
+      }
+    } else {
+      if err := tarWrite(path, tw, stat); err != nil {
+        return err
+      }
+    }
+  }
+  return nil
+}
+
+func closeConn() {
+  if err := l.Close(); err != nil {
+    errLog.Fatalln(err)
+  }
+  outLog.Println("closing connection")
+}
